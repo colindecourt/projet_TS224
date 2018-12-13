2c212805eb22a6b7db19b33647a0aafcb5d92334
JUJUJUJUUU <jujuk33@gmail.com>
2018-12-13 23:55:11 +0100

    Revert "Affichage tendances loc MAIS pas bon alpha"
    This reverts commit 91a265e908253c6b994f084de3137b48db16af3d.

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 clear; close all; clc; dbstop if error;
 
 %% Chargement des données 
-%load('dataEEG.mat');
+load('dataEEG.mat');
 
 %% Déclaration des variables
 Fe=1000; %Hz
@@ -16,14 +16,14 @@ y1=randn(1,1000);
 % x=(1:N)*Te;
 % y=cos(2*pi*f0*x);
 
-% y1=cell2mat(dataEEG(1,1,1))';
-% y2=cell2mat(dataEEG(1,2,2))';
+%y1=cell2mat(dataEEG(1,1,1))';
+y2=cell2mat(dataEEG(1,2,2))';
 
 
 %% Représentation des signaux 
 
-% representation_temp_freq(y1, Fe, 1);
-% representation_temp_freq(y2, Fe, 2);
+representation_temp_freq(y1, Fe, 1);
+representation_temp_freq(y2, Fe, 2);
 
 %% DFA
 
