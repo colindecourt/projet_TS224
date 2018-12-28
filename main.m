3471e814a980f8ced0cf7219c57a47bb9c4fc80e
JUJUJUJUUU <jujuk33@gmail.com>
2018-12-28 11:28:25 +0100

    Rapport img spectro

--- main.m
+++ main.m
@@ -12,22 +12,29 @@ N_vec = [11,13,17,21,27,35,47,59,77,101];
 
 %% Génération des signaux
 
-% x=(1:N)*Te;
-% y=cos(2*pi*f0*x);
+y_bb=randn(1,1000);
 
-% y2=cell2mat(dataEEG(1,2,2))';
+x=(1:N)*Te;
+y_cos=cos(2*pi*f0*x);
+
+y1=cell2mat(dataEEG(1,2,2))';
+
+y2=cell2mat(dataEEG(2,1,4))';
 
 
 %% Représentation des signaux
 
 % representation_temp_freq(y1, Fe, 1);
 % representation_temp_freq(y2, Fe, 2);
+% representation_temp_freq(y_bb, Fe, 3);
+% representation_temp_freq(y_cos, Fe, 4);
 
 %% DFA
 
-y1=cell2mat(dataEEG(2,1,4))';
+%y1=cell2mat(dataEEG(2,1,4))';
 
-[y_init,alpha_DFA]=DFA(y1, N_vec);
+%[y_init,alpha_DFA]=DFA(y1, N_vec);
 
 
-
+%% DMA
+%alpha_DMA=DMA(y1,Fe, N_vec)
--- representation_temp_freq.m
+++ representation_temp_freq.m
@@ -1,30 +1,35 @@
 function [ ] = representation_temp_freq( x, fe, nb_signal)
-% Répresentation temporelle et temps fréquence d'un signal 
-  % Representation temps-fréquence permet de voir la richesse fréquentielle du signal au cours du temps
+% Répresentation temporelle et temps fréquence d'un signal
+% Representation temps-fréquence permet de voir la richesse fréquentielle du signal au cours du temps
 
-Te=1/fe; 
+Te=1/fe;
 N=length(x);
 
-% Axe temporel 
+% Axe temporel
 axe_temp=(1:N)*Te;
 
 % Axe fréquentiel
-axe_freq=(0:N-1)*fe/N; 
+axe_freq=(0:N-1)*fe/N;
 
 % Signal en fréquence
-x_freq=abs(fftshift(fft(x))); 
+x_freq=abs(fftshift(fft(x)));
 
 % Représentations
 figure
 subplot(2,1,1);
 plot(axe_temp,x);
 if nb_signal==1
-title('Représentation du signal EEG(1,1,1) en temps');
+    title('Représentation du signal EEG(1,2,2) en temps');
+elseif nb_signal==2
+        title('Représentation du signal EEG(2,1,4) en temps');
+elseif nb_signal==3
+        title('Représentation d''un bruit blanc gaussien en temps');
 else 
- title('Représentation du signal EEG(1,2,2) en temps');
+    title('Représentation d''un cosinus en temps');
 end
-xlabel('Temps (s)') 
-ylabel('Amplitude') 
+
+xlabel('Temps (s)')
+ylabel('Amplitude')
 
 subplot(2,1,2);
 Nx = length(x);
@@ -35,10 +40,14 @@ nff = max(256,2^nextpow2(nsc)); % nfft
 spectrogram(x,hamming(nsc),nov,nff, fe, 'yaxis');
 %spectrogram(x,hamming(10),nov,nff, fe, 'yaxis');
 if nb_signal==1
-    title('Représentation du signal  EEG(1,1,1) en temps-fréquence');
-else
     title('Représentation du signal  EEG(1,2,2) en temps-fréquence');
+elseif nb_signal==2
+        title('Représentation du signal EEG(2,1,4) en temps-fréquence');
+elseif nb_signal==3
+        title('Représentation d''un bruit blanc gaussien en temps-fréquence');
+else 
+    title('Représentation d''un cosinus en temps-fréquence');
 end
 
 end
-
+    
